3ed6f4729836b5b91c19cd5f307033a710061003
Eduardo Garro <efgarro@yahoo.com>
2026-09-15 15:48:23 -0600

    0915.2

--- src/hooks/useImageModalWidth.ts
+++ src/hooks/useImageModalWidth.ts
@@ -15,8 +15,8 @@ function computeImageModalWidth(
 
   if (isPortrait) {
     if (screenWidth <= 575) return Math.round(340);
-    if (screenWidth <= 767) return Math.round(440);
-    return Math.round(600 / 1.5);
+    if (screenWidth <= 767) return Math.round(340);
+    return Math.round(400);
   } else {
     // treat everything else as landscape
     if (screenWidth <= 575) return Math.round(340);
